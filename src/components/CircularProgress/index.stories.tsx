f9048bf653d96d09e0a28f653c03496fdb105050
Pallab Bharman <bharmanpallab@gmail.com>
2025-08-19 18:19:08 +0600

    storybook added

---
+++ src/components/CircularProgress/index.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import CircularProgress from '.';
+
+const meta: Meta<typeof CircularProgress> = {
+    component: CircularProgress,
+    parameters: {
+        layout: 'centered',
+        docs: {
+            description: {
+                component:
+                    'CircularProgress is used when retrieving data or performing slow computations, and help to notify users that loading is underway.',
+            },
+        },
+    },
+    tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {},
+};
+
+export const Small: Story = {
+    args: {
+        size: 20,
+    },
+};
+
+export const Large: Story = {
+    args: {
+        size: 40,
+    },
+};
